66c58b869d0b6af455f156a634f5aed995a98b7f
valentinfrlch <valentin.froehlich@gmail.com>
2023-07-05 15:57:30 +0200

    added training and validation TimeSeries

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 # libraries:
 import numpy as np
 import pandas as pd
-from pytorch_forecasting import TimeSeriesDataSet
+import torch
+from pytorch_forecasting import TimeSeriesDataSet, GroupNormalizer, Baseline
 
 
 def preprocess():
@@ -49,12 +50,45 @@ def train(data):
         allow_missing_timesteps=True,
         target_normalizer=None
     )
+    
+    max_prediction_length = 24
+    max_encoder_length = 7*24
+    print("********")
+    print(dataset["time_idx"])
+    print("********")
+    training_cutoff = dataset["time_idx"].max() - max_prediction_length
+    
+    training = TimeSeriesDataSet(
+        dataset[lambda x: x.hours_from_start <= training_cutoff],
+        time_idx="time_idx",
+        target="rtn",
+        group_ids=["uid"],
+        min_encoder_length=max_encoder_length // 2, 
+        max_encoder_length=max_encoder_length,
+        min_prediction_length=1,
+        max_prediction_length=max_prediction_length,
+        time_varying_known_reals=["time_idx"],
+        time_varying_unknown_reals=['rtn'],
+        target_normalizer=GroupNormalizer(
+            groups=["uid"], transformation="softplus"
+        ),  # we normalize by group
+        add_relative_time_idx=True,
+        add_target_scales=True,
+        add_encoder_length=True,
+    )
+    
+    validation = TimeSeriesDataSet.from_dataset(training, dataset, predict=True, stop_randomization=True)
+    
+    # create dataloaders for  our model
+    batch_size = 64 
+    # if you have a strong GPU, feel free to increase the number of workers  
+    train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=2)
+    validation_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=2)
 
-    # pass to dataloader
-    dataloader = dataset.to_dataloader(batch_size=1)
 
-    #load the first batch
-    x, y = next(iter(dataloader))
+    actuals = torch.cat([y for x, (y, weight) in iter(validation_dataloader)]).to("cuda")
+    baseline_predictions = Baseline().predict(validation_dataloader)
+    (actuals - baseline_predictions).abs().mean().item()
     
     
     
