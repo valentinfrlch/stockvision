ddbbe5ea76574cfbc4c62163fbcff4fdbee8a4db
valentinfrlch <valentin.froehlich@gmail.com>
2023-07-05 18:05:56 +0200

    training

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import numpy as np
 import pandas as pd
 import torch
 import matplotlib.pyplot as plt
-from pytorch_forecasting import TimeSeriesDataSet, GroupNormalizer, Baseline
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import EarlyStopping, LearningRateMonitor
+from lightning.pytorch.loggers import TensorBoardLogger
+from pytorch_forecasting import TimeSeriesDataSet, GroupNormalizer, Baseline, TemporalFusionTransformer, QuantileLoss
 
 
 def preprocess():
@@ -79,19 +82,53 @@ def train(data):
     
     validation = TimeSeriesDataSet.from_dataset(training, data, predict=True, stop_randomization=True)
     
-    # create dataloaders for  our model
-    batch_size = 64 
-    # if you have a strong GPU, feel free to increase the number of workers  
-    train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=12)
+    # create dataloaders
+    batch_size = 64  
+    training_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=12)
     validation_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=12)
 
 
-    torch.set_float32_matmul_precision('medium')                                                                             # todo: set to 'high'
-
+    torch.set_float32_matmul_precision('medium') # todo: set to 'high'
     actuals = torch.cat([y for x, (y, weight) in iter(validation_dataloader)]).to("cuda")
     baseline_predictions = Baseline().predict(validation_dataloader)
     (actuals - baseline_predictions).abs().mean().item()
     
+    early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=5, verbose=True, mode="min")
+    lr_logger = LearningRateMonitor()  
+    logger = TensorBoardLogger("lightning_logs")  
+
+    trainer = pl.Trainer(
+        max_epochs=45,
+        accelerator='gpu', 
+        devices=1,
+        enable_model_summary=True,
+        gradient_clip_val=0.1,
+        callbacks=[lr_logger, early_stop_callback],
+        logger=logger)
+
+    tft = TemporalFusionTransformer.from_dataset(
+        training,
+        learning_rate=0.001,
+        hidden_size=160,
+        attention_head_size=4,
+        dropout=0.1,
+        hidden_continuous_size=160,
+        output_size=7,  # there are 7 quantiles by default: [0.02, 0.1, 0.25, 0.5, 0.75, 0.9, 0.98]
+        loss=QuantileLoss(),
+        log_interval=10, 
+        reduce_on_plateau_patience=4)
+
+    trainer.fit(
+        tft,
+        train_dataloaders=training_dataloader,
+        val_dataloaders=validation_dataloader)
+    
+    
+    best_model_path = trainer.checkpoint_callback.best_model_path
+    print(best_model_path)
+    best_tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
+        
+     
     
     
     
