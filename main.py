018cd5c61f1d189128da35c8182e5657293226f2
valentinfrlch <valentin.froehlich@gmail.com>
2023-07-06 16:09:10 +0200

    code cleanup

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def train(data):
         attention_head_size=4,
         dropout=0.1,
         hidden_continuous_size=160,
-        output_size=7,  # there are 7 quantiles by default: [0.02, 0.1, 0.25, 0.5, 0.75, 0.9, 0.98]
+        output_size=7,
         loss=QuantileLoss(),
         log_interval=10, 
         reduce_on_plateau_patience=4)
