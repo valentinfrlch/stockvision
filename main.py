fe64b0f7cf39ba05102d97d017c403f2d5b2e273
valentinfrlch <valentin.froehlich@gmail.com>
2023-07-08 08:40:15 +0200

    updated forecasting()

--- main.py
+++ main.py
@@ -18,27 +18,24 @@ def preprocess():
     path = "dataset/returns.csv"
     # the limiter is a semicolon
     df = pd.read_csv(path, sep=";")
-    
+
     # align all the stocks by date
     df_time = pd.DataFrame({"Date": df.Date.unique()})
     df_time.sort_values(by="Date", inplace=True)
     df_time.reset_index(drop=True, inplace=True)
     df_time["idx"] = list(df_time.index)
     df = pd.merge(df, df_time, on=["Date"], how="inner")
-    
-    print(df.head(10))
-    
-    
+
     data = pd.DataFrame(
         dict(
             # convert Date to datetime
-            date = lambda_date(df["Date"]),
-            time_idx = df.index,
-            uid = df["UID"],
-            rtn = df["Return"],
+            date=lambda_date(df["Date"]),
+            time_idx=df.index,
+            uid=df["UID"],
+            rtn=df["Return"],
         )
     )
-    
+
     return data
 
 
@@ -49,9 +46,10 @@ def lambda_date(dates):
         dates[i] = pd.to_datetime(str(dates[i]), format="%Y%m%d")
     return dates
 
+
 def visualize(data):
     # use matplotlib to visualize the data, different stocks have different colors
-    
+
     plt.figure(figsize=(15, 8))
     for uid in data["uid"].unique():
         plt.plot(
@@ -64,18 +62,17 @@ def visualize(data):
     plt.show()
 
 
-
-def train(data):    
-    max_prediction_length = 24
-    max_encoder_length = 7*24
+def train(data):
+    max_prediction_length = 30
+    max_encoder_length = 90
     training_cutoff = data["time_idx"].max() - max_prediction_length
-    
+
     training = TimeSeriesDataSet(
         data[lambda x: x.time_idx <= training_cutoff],
         time_idx="time_idx",
         target="rtn",
         group_ids=["uid"],
-        min_encoder_length=max_encoder_length // 2, 
+        min_encoder_length=max_encoder_length // 2,
         max_encoder_length=max_encoder_length,
         min_prediction_length=1,
         max_prediction_length=max_prediction_length,
@@ -88,27 +85,31 @@ def train(data):
         add_target_scales=True,
         add_encoder_length=True,
     )
-    
-    validation = TimeSeriesDataSet.from_dataset(training, data, predict=True, stop_randomization=True)
-    
+
+    validation = TimeSeriesDataSet.from_dataset(
+        training, data, predict=True, stop_randomization=True)
+
     # create dataloaders
-    batch_size = 64  
-    training_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=12)
-    validation_dataloader = validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=12)
+    batch_size = 64
+    training_dataloader = training.to_dataloader(
+        train=True, batch_size=batch_size, num_workers=12)
+    validation_dataloader = validation.to_dataloader(
+        train=False, batch_size=batch_size * 10, num_workers=12)
 
-
-    torch.set_float32_matmul_precision('medium') # todo: set to 'high'
-    actuals = torch.cat([y for x, (y, weight) in iter(validation_dataloader)]).to("cuda")
+    torch.set_float32_matmul_precision('medium')  # todo: set to 'high'
+    actuals = torch.cat(
+        [y for x, (y, weight) in iter(validation_dataloader)]).to("cuda")
     baseline_predictions = Baseline().predict(validation_dataloader)
     (actuals - baseline_predictions).abs().mean().item()
-    
-    early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=5, verbose=True, mode="min")
-    lr_logger = LearningRateMonitor()  
-    logger = TensorBoardLogger("lightning_logs")  
+
+    early_stop_callback = EarlyStopping(
+        monitor="val_loss", min_delta=1e-4, patience=5, verbose=True, mode="min")
+    lr_logger = LearningRateMonitor()
+    logger = TensorBoardLogger("lightning_logs")
 
     trainer = pl.Trainer(
         max_epochs=5,
-        accelerator='gpu', 
+        accelerator='gpu',
         devices=1,
         enable_model_summary=True,
         gradient_clip_val=0.1,
@@ -124,34 +125,70 @@ def train(data):
         hidden_continuous_size=160,
         output_size=7,
         loss=QuantileLoss(),
-        log_interval=10, 
+        log_interval=10,
         reduce_on_plateau_patience=4)
 
     trainer.fit(
         tft,
         train_dataloaders=training_dataloader,
         val_dataloaders=validation_dataloader)
-    
-    
+
     best_model_path = trainer.checkpoint_callback.best_model_path
-    print(best_model_path)
     best_tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
-    return best_tft, validation_dataloader, 
-        
-     
-    
-    
-    
-def forecast(lookback, forward, best_tft, validation_dataloader):
+    return best_tft, validation_dataloader,
+
+
+def forecast(lookback, forward, data):
     # predict without retraining:
     # load the best model from the checkpoint
-    model = TemporalFusionTransformer.load_from_checkpoint("lightning_logs/lightning_logs/version_0/checkpoints/epoch=4-step=9985.ckpt")
-    
-    
-    
+    model = TemporalFusionTransformer.load_from_checkpoint(
+        "lightning_logs/lightning_logs/version_0/checkpoints/epoch=4-step=9985.ckpt")
+
+    # create a dataset with the lookback period and create a dataloader
+
+    lookback_ds = TimeSeriesDataSet(
+        data[lambda x: x.time_idx >= data["time_idx"].max() - lookback],
+        time_idx="time_idx",
+        target="rtn",
+        group_ids=["uid"],
+        min_encoder_length=forward // 2,
+        max_encoder_length=forward,
+        min_prediction_length=forward,
+        max_prediction_length=forward,
+        time_varying_known_reals=["time_idx"],
+        time_varying_unknown_reals=['rtn'],
+        target_normalizer=GroupNormalizer(
+            groups=["uid"], transformation="count"
+        ),  # we normalize by group
+        add_relative_time_idx=True,
+        add_target_scales=True,
+        add_encoder_length=True,
+        allow_missing_timesteps=True,
+    )
+
+    # create a dataloader
+    predict_dataloader = lookback_ds.to_dataloader(
+        train=False, batch_size=64, num_workers=12
+    )
+
+    # predict
+    torch.set_float32_matmul_precision('medium')
+    predictions = model.predict(predict_dataloader)
+
+    # Use Tensor.cpu() to copy the tensor to host memory first.
+    predictions = predictions.cpu().numpy()
+
+    # reshape the predictions
+    predictions = predictions.reshape(-1, forward)
+
+    # convert the predictions to a dataframe
+    predictions = pd.DataFrame(predictions)
+
+    return predictions
 
 
 if __name__ == "__main__":
     data = preprocess()
-    train(data)
-    
+    # train(data)
+    prediction = forecast(lookback=30, forward=30, data=data)
+    print(prediction)
